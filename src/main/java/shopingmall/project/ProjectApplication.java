45ddd7dacebe66ecd17929cca748ffd96b06145f
ldh7043 <ldh7043@gmail.com>
2023-04-18 23:16:13 +0900

    baseentity 빈 추가

--- src/main/java/shopingmall/project/ProjectApplication.java
+++ src/main/java/shopingmall/project/ProjectApplication.java
@@ -2,7 +2,14 @@ package shopingmall.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing // BaseEntity 동작
 @SpringBootApplication
 public class ProjectApplication {
 
@@ -10,4 +17,10 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		// baseEntity에서 createdBy, lastModifiedBy 에 아이디 값 넣기용
+		// springsecurity써서 아이디같은 세션정보 꺼내와야함 아래는 테스트용 Optional.of(id) get으로 아이디 꺼내오기
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
