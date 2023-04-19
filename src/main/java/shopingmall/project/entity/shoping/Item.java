c7cd1d1bbcd729639a522fafdfc147f34de15b29
ldh7043 <ldh7043@gmail.com>
2023-04-19 23:24:21 +0900

    상품 엔티티 추가

---
+++ src/main/java/shopingmall/project/entity/shoping/Item.java
@@ -0,0 +1,45 @@
+package shopingmall.project.entity.shoping;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shopingmall.project.entity.baseEntity.BaseEntity;
+import shopingmall.project.type.ItemType;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
+public class Item extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;            //seq
+    private String name;        //상품명
+    private int price;          //상품가격
+
+    @Enumerated(EnumType.STRING)
+    private ItemType item;      //상품종류
+    private String description; //상품설명
+    private int stockQuantity;  //상품수량
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0) {
+//            throw new NotEnoughStockException("수량이 부족합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
+}
---
+++ src/main/java/shopingmall/project/type/ItemType.java
@@ -0,0 +1,7 @@
+package shopingmall.project.type;
+
+public enum ItemType {
+
+    FOOD, CLOTHES
+    ;
+}
