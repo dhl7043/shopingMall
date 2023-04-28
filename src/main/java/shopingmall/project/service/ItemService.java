b8156d9509e34877070550640f58e0df64def852
ldh7043 <ldh7043@gmail.com>
2023-04-28 23:20:09 +0900

    itemService 추가

---
+++ src/main/java/shopingmall/project/service/ItemService.java
@@ -0,0 +1,38 @@
+package shopingmall.project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shopingmall.project.entity.shoping.Item;
+import shopingmall.project.repository.ItemRepository;
+import shopingmall.project.type.ItemType;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> findByItemId(Long itemId) {
+        return itemRepository.findById(itemId);
+    }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, ItemType item, String description, int stockQuantity) {
+        Optional<Item> changeItem = itemRepository.findById(id);
+        changeItem.get().changeItem(name, price, item, description, stockQuantity);
+    }
+}
