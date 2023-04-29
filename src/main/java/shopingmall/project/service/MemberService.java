48931ea6d97868941b74b1e81d7466f2a08a59c7
ldh7043 <ldh7043@gmail.com>
2023-04-29 23:30:52 +0900

    MemberService 추가

---
+++ src/main/java/shopingmall/project/service/MemberService.java
@@ -0,0 +1,51 @@
+package shopingmall.project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shopingmall.project.entity.shoping.Address;
+import shopingmall.project.entity.shoping.Member;
+import shopingmall.project.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        duplicateCheckMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    @Transactional
+    public void updateMember(Long id, String name, int age, String phoneNumber, String email, String city, String street, String zipcode) {
+        Optional<Member> member = memberRepository.findById(id);
+        member.get().changeMember(name, age, phoneNumber, email, new Address(city, street, zipcode));
+    }
+
+    private void duplicateCheckMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+}
