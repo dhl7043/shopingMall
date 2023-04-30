3204c86ff03f2e97c0de6153db5a6f3a70bd0e73
ldh7043 <ldh7043@gmail.com>
2023-04-30 23:19:09 +0900

    QClass 추가 MemberJpaRepository 수정

--- src/main/java/shopingmall/project/repository/MemberJpaRepository.java
+++ src/main/java/shopingmall/project/repository/MemberJpaRepository.java
@@ -1,6 +1,7 @@
 package shopingmall.project.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import shopingmall.project.entity.shoping.Member;
@@ -12,6 +13,8 @@ public class MemberJpaRepository {
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
+    QMember qMember = QMember.member;
+
     public MemberJpaRepository(EntityManager em) {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
@@ -19,5 +22,11 @@ public class MemberJpaRepository {
 
     public void updateMemberName(Member member) {
 
+        JPAUpdateClause updateMember = queryFactory
+                .update(qMember)
+//                .set()
+                .where(qMember.id.eq(member.getId()));
+
+        updateMember.execute();
     }
 }
