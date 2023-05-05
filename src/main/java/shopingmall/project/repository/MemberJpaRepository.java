4bec86ce967da611249b6bcead2b605cc7e0ce4a
ldh7043 <ldh7043@gmail.com>
2023-05-05 21:48:59 +0900

    쿼리문 수정 memberJpaRepository 수정

--- src/main/java/shopingmall/project/repository/MemberJpaRepository.java
+++ src/main/java/shopingmall/project/repository/MemberJpaRepository.java
@@ -4,7 +4,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.jpa.impl.JPAUpdateClause;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import shopingmall.project.entity.shoping.Member;
+import shopingmall.project.entity.shoping.QMember;
 
 import java.util.Optional;
 
@@ -14,21 +16,26 @@ public class MemberJpaRepository {
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
-//    QMember qMember = QMember.member;
+    QMember qMember = QMember.member;
 
     public MemberJpaRepository(EntityManager em) {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public void updateMemberName(Optional<Member> member) {
+    @Transactional
+    public void updateMember(Optional<Member> member) {
 
-//        JPAUpdateClause updateMember = queryFactory
-//                .update(qMember)
-////                .set()
-//                .where(qMember.id.eq(member.get().getId()));
-//
-//        updateMember.execute();
+        JPAUpdateClause updateMember = queryFactory
+                .update(qMember)
+                .set(qMember.name, member.get().getName())
+                .set(qMember.age, member.get().getAge())
+                .set(qMember.phoneNumber, member.get().getPhoneNumber())
+                .set(qMember.email, member.get().getEmail())
+                .set(qMember.address, member.get().getAddress())
+                .where(qMember.id.eq(member.get().getId()));
+
+        updateMember.execute();
     }
 
 }
