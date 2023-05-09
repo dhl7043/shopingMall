1bb69281a1fa0508e9be0b31e033d09e69949dc6
ldh7043 <ldh7043@gmail.com>
2023-05-09 23:40:54 +0900

    memberSearchCondition 생성

---
+++ src/main/java/shopingmall/project/request/MemberSearchCondition.java
@@ -0,0 +1,11 @@
+package shopingmall.project.request;
+
+/**
+ * 회원명, 나이
+ */
+public class MemberSearchCondition {
+
+    private String username;
+    private String ageGoe;
+    private String ageLoe;
+}
