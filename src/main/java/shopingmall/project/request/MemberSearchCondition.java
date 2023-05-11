ec19c0183a31ac7d9000564c08c773b068975c99
ldh7043 <ldh7043@gmail.com>
2023-05-11 22:35:17 +0900

    조회조건 수정 membersearchcondition 수정

--- src/main/java/shopingmall/project/request/MemberSearchCondition.java
+++ src/main/java/shopingmall/project/request/MemberSearchCondition.java
@@ -1,11 +1,15 @@
 package shopingmall.project.request;
 
+import lombok.Data;
+import lombok.Getter;
+
 /**
  * 회원명, 나이
  */
+@Getter
 public class MemberSearchCondition {
 
     private String username;
-    private String ageGoe;
-    private String ageLoe;
+    private Integer ageGoe;
+    private Integer ageLoe;
 }
