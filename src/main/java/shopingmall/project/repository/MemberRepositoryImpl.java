b52c058873290ee9d7ac86c75112db06f3b15d0e
ldh7043 <ldh7043@gmail.com>
2023-05-12 21:19:52 +0900

    memberRepositoryImpl 수정 - memberSearch 추가

--- src/main/java/shopingmall/project/repository/MemberRepositoryImpl.java
+++ src/main/java/shopingmall/project/repository/MemberRepositoryImpl.java
@@ -1,11 +1,21 @@
 package shopingmall.project.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import shopingmall.project.entity.shoping.Member;
 import shopingmall.project.request.MemberSearchCondition;
 import shopingmall.project.response.MemberResponse;
+import shopingmall.project.response.QMemberResponse;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static shopingmall.project.entity.shoping.QMember.*;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
@@ -17,6 +27,43 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     @Override
     public Page<MemberResponse> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
-        return null;
+        List<MemberResponse> content = queryFactory
+                .select(new QMemberResponse(
+                        member.id.as("memberId"),
+                        member.name.as("username"),
+                        member.age,
+                        member.phoneNumber,
+                        member.email,
+                        member.address.city,
+                        member.address.street,
+                        member.address.zipcode))
+                .from(member)
+                .where(usernameEq(condition.getUsername()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .where(usernameEq(condition.getUsername()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.name.eq(username) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 }
