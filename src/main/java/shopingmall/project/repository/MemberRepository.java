db15ca8f9e042870a5d18c12a4415fd728f9cd31
ldh7043 <ldh7043@gmail.com>
2023-05-13 23:43:15 +0900

    memberrepository 수정 - memberdto 삭제

--- src/main/java/shopingmall/project/repository/MemberRepository.java
+++ src/main/java/shopingmall/project/repository/MemberRepository.java
@@ -3,10 +3,11 @@ package shopingmall.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shopingmall.project.dto.MemberDto;
 import shopingmall.project.entity.shoping.Member;
+import shopingmall.project.request.MemberCreate;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<MemberDto> findByName(String name);
+    List<MemberCreate> findByName(String name);
 }
