861bc2fdfe1c6293971230da5854b25b5340aba9
ldh7043 <ldh7043@gmail.com>
2023-06-04 22:51:56 +0900

    memberEdit 추가

---
+++ src/main/java/shopingmall/project/request/MemberEdit.java
@@ -0,0 +1,29 @@
+package shopingmall.project.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import shopingmall.project.entity.shoping.Address;
+
+/**
+ * 회원 정보 수정
+ * email -> id 수정불가
+ */
+
+@Getter
+@ToString
+public class MemberEdit {
+
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private Address address;
+
+    @Builder
+    public MemberEdit(String name, int age, String phoneNumber, Address address) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
