4df2a791005713b947eb41cdf470eee0d3c99f3f
ldh7043 <ldh7043@gmail.com>
2023-06-05 14:18:52 +0900

    validation 추가

--- src/main/java/shopingmall/project/request/ItemCreate.java
+++ src/main/java/shopingmall/project/request/ItemCreate.java
@@ -1,5 +1,6 @@
 package shopingmall.project.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import shopingmall.project.type.ItemType;
@@ -7,10 +8,15 @@ import shopingmall.project.type.ItemType;
 @Getter
 public class ItemCreate {
 
+    @NotBlank(message = "상품명을 입력해주세요")
     private String name;
+    @NotBlank(message = "가격을 입력해주세요")
     private int price;
+    @NotBlank(message = "상품 종류를 선택해주세요")
     private ItemType itemType;
+    @NotBlank(message = "상품 설명을 입력해주세요")
     private String description;
+    @NotBlank(message = "상품 수량을 입력해주세요")
     private int stockQuantity;
 
     @Builder
