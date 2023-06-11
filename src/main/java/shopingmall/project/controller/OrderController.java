0c45f9113f00c780d61cf284ab84e0e758d55cda
ldh7043 <ldh7043@gmail.com>
2023-06-11 16:04:57 +0900

    OrderController 수정 - 주문하기, 주문취소 추가

--- src/main/java/shopingmall/project/controller/OrderController.java
+++ src/main/java/shopingmall/project/controller/OrderController.java
@@ -1,7 +1,7 @@
 package shopingmall.project.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shopingmall.project.service.OrderService;
 
 @RestController
@@ -11,7 +11,18 @@ public class OrderController {
     private final OrderService orderService;
 
     /* 주문하기 */
+    @PostMapping("/order")
+    public void order(@RequestParam Long memberId,
+                      @RequestParam Long itemId,
+                      @RequestParam int count) {
+        orderService.order(memberId, itemId, count);
+    }
+
     /* 주문 취소 */
+    @PostMapping("/orders/{orderId}/cancel")
+    public void cancelOrder(@PathVariable Long orderId) {
+        orderService.cancelOrder(orderId);
+    }
     /* 주문 조회 */
     /* 주문 내역 */
 }
